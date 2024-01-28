cf77b74e9ebf75401fd9cdcbe11cd30eb0694dbe
prateek-narsinghani <prateek.narsinghani@gmail.com>
2024-01-28 14:40:10 +0530

    add default for each form elements

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, SubmitField
 
 class SearchForm(FlaskForm):
-    search_by = SelectField('Search By', choices=[('title', 'Title'), ('channel', 'Channel')])
+    search_by = SelectField('Search By', choices=[('title', 'Title'), ('channel', 'Channel')], default='title')
     search_query = StringField('Search Query')
-    order_by = SelectField('Order By', choices=[('published_at', 'Published At')])
+    order_by = SelectField('Order By', choices=[('published_at', 'Published At')], default='published_at')
     sort = SelectField('Sort', choices=[('desc', 'Descending'), ('asc', 'Ascending')], default='desc')
     submit = SubmitField('Search')
